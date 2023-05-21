0b66bdc21af2e7b700c379ba7ca5d1647ea2261c
Carlos Arturo Diaz <70271822+ArturoDiaz02@users.noreply.github.com>
2023-05-20 23:19:55 -0500

    iterativo

--- deployClient.sh
+++ deployClient.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export PASSWORD="swarch"
 path="ArturoDiaz-DanielaBonilla-CallBack_Client"
-exec 3<"./host.txt"
+exec 3<"host.txt"
 while read -u 3 line; do
     ip=$(echo "$line" | awk '{print $1}')
     name=$(echo "$line" | awk '{print $2}')
-    echo "host swarch@"$ip""
+    echo "host swarch@"$name""
     sed -i "s/^CallbackReceiver.Endpoints = tcp -h hgrid3 -p 10008/CallbackReceiver.Endpoints=default -h $ip -p 10008/" client/src/main/resources/config.client
     gradle -p client build
     sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no swarch@"$ip" "if [ -d $path ]; then echo 'Folder exists'; else mkdir $path; fi"
