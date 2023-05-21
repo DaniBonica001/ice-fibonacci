6c5691b9c7c95b21accfa9c431e5651072e0200f
Carlos Arturo Diaz <70271822+ArturoDiaz02@users.noreply.github.com>
2023-05-20 23:05:47 -0500

    deploy

--- deployServer.sh
+++ deployServer.sh
@@ -4,4 +4,5 @@ server_id="xhgrid1"
 path="ArturoDiaz-DanielaBonilla-CallBack_Server"
 sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no swarch@"$server_id" "if [ -d $path ]; then echo 'Folder exists'; else mkdir $path; fi"
 echo "Send Jar Server"
+gradle -p server build
 sshpass -p ${PASSWORD} scp -o StrictHostKeyChecking=no ./server/build/libs/server.jar swarch@"$server_id":./$path
